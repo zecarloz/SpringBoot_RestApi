6d158c48968541d26ff3d7c602124605f2ed3657
zecarloz <josecarlos2503@hotmail.com>
2021-02-08 15:53:37 -0300

    API Spring final

---
+++ produto/src/main/java/com/api/produto/modelo/RespostaModelo.java
@@ -0,0 +1,15 @@
+package com.api.produto.modelo;
+
+public class RespostaModelo {
+	// Atributo
+	private String mensagem;
+	
+	// get e set
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+}
